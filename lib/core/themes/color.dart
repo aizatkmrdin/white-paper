018f1f846f90fde774546b2164cfc9a54cfce8d0
aizatpadimas <aizatpadimas@gmail.com>
2023-12-07 16:08:47 +0800

    core: add default color & assets: add fonts

---
+++ lib/core/themes/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  Color background = const Color(0xFFFAFAFA);
+  Color primary = const Color(0xFF2E3234);
+  Color secondary = const Color(0xFFB8B9BA);
+  Color selected = const Color(0xFFEFD265);
+  Color shadow = const Color(0xFFEBECEC);
+}
